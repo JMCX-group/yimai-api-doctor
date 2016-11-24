d5bb216041e9390e48f97ea81e9a4b03fc2f6e88
liyingxuan <44600937@qq.com>
2016-11-24 17:51:56 +0800

    更新关于我们的图片

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,20 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
-
 class AboutController extends Controller
 {
     public function contactUs()
     {
         return view('about.contact_us');
     }
-    
+
     public function introduction()
     {
         return view('about.introduction');
     }
-    
+
     public function lawyer()
     {
         return view('about.lawyer');
