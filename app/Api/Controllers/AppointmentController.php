4af03bc31cdbd0f810791f4432ff5f9d3fc65fab
liyingxuan <44600937@qq.com>
2017-01-15 16:31:02 +0800

    修改get()和first()那点事

--- app/Api/Controllers/AppointmentController.php
+++ app/Api/Controllers/AppointmentController.php
@@ -144,7 +144,7 @@ class AppointmentController extends BaseController
              * 注册患者发送单播通知；未注册患者需要发送短信
              */
             $patient = Patient::where('phone', $request['phone'])->first();
-            if ($patient->isEmpty()) {
+            if (isset($patient->id)) {
                 $this->sendSMS($user, $doctor, $request['phone']);
             } else {
                 if ($patient->device_token != '' && $patient->device_token != null) {
