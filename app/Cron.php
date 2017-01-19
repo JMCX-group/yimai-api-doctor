893b07a7bbf4ba6c88f4879f144fbc722e75a634
liyingxuan <44600937@qq.com>
2017-01-19 14:29:57 +0800

    cron test

---
+++ app/Cron.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cron extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'cron_test';
+}
