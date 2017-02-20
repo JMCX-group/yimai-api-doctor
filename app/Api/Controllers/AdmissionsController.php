d24f96e9b9e712cb05b78a1c081cab6f666019d9
lyx <44600937@qq.com>
2017-02-20 23:21:33 +0800

    更新时间格式未传正确的处理机制

--- app/Api/Controllers/AdmissionsController.php
+++ app/Api/Controllers/AdmissionsController.php
@@ -303,7 +303,7 @@ class AdmissionsController extends BaseController
         if ($visitTime == '1970-01-01 00:00:00') {
             $visitTime = $request['visit_time'];
         }
-        Log::info('android-rescheduled-time', ['context' => $request['visit_time']]);
+        Log::info('android-rescheduled-time', ['context' => $request['visit_time'] . ' - new:' . $visitTime]);
 
         $appointment = Appointment::find($request['id']);
         $appointment->status = 'wait-4'; //医生改期
